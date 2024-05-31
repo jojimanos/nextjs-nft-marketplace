414da2d4b06b39c119779d9808ed3e91d0184e74
jojimanos <manosgeorge7@gmail.com>
2024-05-31 19:42:51 +0300

    Tailwind css classess to the Header contents.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="NFT Marketplace"/>
-        </Head>
       <body>
+        <Head>
+          <title>NFT Marketplace</title>
+          <meta name="NFT Marketplace" content="NFT Marketplace"/>
+        </Head>
         <Main />
         <NextScript />
       </body>
--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -5,14 +5,17 @@ import Link from "next/link"
 
 const Header = () => {
     return (
-        <nav>
-            <Link href="/">
-            <p>NFT Marketplace</p>
-            </Link>
-            <Link href="/sell-nft">
-            <p>Sell NFT</p>
-            </Link>
-            <ConnectButton />
+        <nav className="p-5 borber-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+            <div className="flex flex-row items center">
+                <Link href="/">
+                    <span className="mr-4 p-6">NFT Marketplace</span>
+                </Link>
+                <Link href="/sell-nft">
+                    <span className="mr-4 p-6">Sell NFT</span>
+                </Link>
+                <ConnectButton moralisAuth={false}/>
+            </div>
         </nav>
     )
 }
